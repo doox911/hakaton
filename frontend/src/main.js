c75cd8d3c09e28b69e3328b78048f639be44c00d
Поляков Андрей <doox911@gmail.com>
2021-10-01 23:30:28 +0300

    Сделал предзагрузку приложения

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,9 +8,28 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App),
-}).$mount('#app');
+/**
+ * @param {function} callback
+ */
+function hidePreloadContainer(callback = () => {}) {
+  const element = document.querySelector('.ipril-container');
+
+  setTimeout(() => {
+    element.classList.add('hide');
+
+    setTimeout(() => {
+      element.remove();
+
+      callback();
+    }, 1000);
+  }, 3000);
+}
+
+hidePreloadContainer(() => {
+  new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App),
+  }).$mount('#app');
+});
